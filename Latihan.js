9abc97bda99a1e5ecf2724cce71372050472724b
Ali Akbar <aliakbar@users-MacBook-Pro.local>
2024-08-07 10:26:14 +0800

    ini adalah aplikasi pertama untuk membuat mobile app dengan react native

---
+++ Latihan.js
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export class Latihan extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Latihan</Text>
+      </View>
+    );
+  }
+}
+
+export default Latihan;
